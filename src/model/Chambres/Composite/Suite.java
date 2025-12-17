d519637f924c0ccf1ab53c9bbc78c73379c849c6
ikramhadjmohamed <ikramhadjmohamed5@gmail.com>
2025-12-17 13:01:58 +0100

    Composite

--- src/model/Chambres/Composite/Suite.java
+++ src/model/Chambres/Composite/Suite.java
@@ -1,15 +1,30 @@
 package model.Chambres.Composite;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Suite extends Chambre {
     
+     private List<Chambre> sousChambres;
     public Suite(int numero, int etage, double prixBase, StatutChambre statut, List<String> equipements) {
         super(numero, etage, prixBase, statut, equipements);
+        sousChambres = new ArrayList<>();
+    }
+
+    public void ajouterChambre(Chambre chambre) {
+        sousChambres.add(chambre);
+    }
+
+    public void retirerChambre(Chambre chambre) {
+    sousChambres.remove(chambre);
     }
 
     @Override
     public double calculerPrix() {
-        return prixBase * 1.5;
+        double total = prixBase;
+        for (Chambre c : sousChambres) {
+            total += c.calculerPrix();
+        }
+        return total;
     }
 }
