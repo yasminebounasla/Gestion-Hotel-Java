0ed8bd06ba172889b922a4e85fa6d172f7f0d944
yasmine bounasla <yasminebounasla@gmail.com>
2025-12-17 12:56:37 +0100

    fix errors

--- src/App.java
+++ src/App.java
@@ -3,6 +3,7 @@ import view.VueChambre;
 import view.VueReservationMenu;
 import view.VueConsole;
 import view.VueStatistiques;
+import view.VueClient;
 
 public class App {
     public static void main(String[] args) {
@@ -12,6 +13,7 @@ public class App {
         VueReservationMenu vueReservation = new VueReservationMenu(controleur.getControleurReservation());
         VueConsole vueConsole = new VueConsole(controleur);
         VueStatistiques vueStats = new VueStatistiques(controleur);
+        VueClient vueClient = new VueClient(controleur.getControleurClient());
 
         // Exemple : on peut juste lancer le menu principal de la console
         int choix = -1;
@@ -20,9 +22,10 @@ public class App {
         while (choix != 0) {
             System.out.println("\n=== Menu Principal ===");
             System.out.println("1. Gérer les chambres");
-            System.out.println("2. Gérer les réservations");
-            System.out.println("3. Autres fonctionnalités");
-            System.out.println("4. Statistiques");
+            System.out.println("2. Gérer les Clients");
+            System.out.println("3. Gérer les réservations");
+            System.out.println("4. Autres fonctionnalités");
+            System.out.println("5. Statistiques");
             System.out.println("0. Quitter");
             System.out.print("Choisissez une option: ");
             choix = scanner.nextInt();
@@ -30,9 +33,10 @@ public class App {
 
             switch (choix) {
                 case 1 -> vueChambre.afficherMenu();
-                case 2 -> vueReservation.afficherMenu();
-                case 3 -> vueConsole.afficher();
-                case 4 -> vueStats.afficher();
+                case 2 -> vueClient.afficherMenu();
+                case 3 -> vueReservation.afficherMenu();
+                case 4 -> vueConsole.afficher();
+                case 5 -> vueStats.afficher();
                 case 0 -> System.out.println("Au revoir !");
                 default -> System.out.println("Option invalide !");
             }
