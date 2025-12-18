3d77d48204400cd5e6216912484080a23a9b2b1b
ikramhadjmohamed <ikramhadjmohamed5@gmail.com>
2025-12-18 18:51:35 +0100

    final update

--- src/model/reservations/observer/Sujet.java
+++ src/model/reservations/observer/Sujet.java
@@ -1,5 +1,15 @@
 package model.Reservations.Observer;
-
+/**
+ * Interface Sujet (pattern Observer).
+ * 
+ * Cette interface représente le "Sujet" observé.
+ * Un sujet est un objet qui contient un état et qui
+ * doit notifier automatiquement les observateurs
+ * lorsqu’un changement se produit.
+ * 
+ * Dans notre projet, le sujet est lié à la gestion
+ * des réservations (ajout, modification, suppression, ajout de service).
+ */
 public interface Sujet {
     void ajouterObservateur(Observateur obs);
     void supprimerObservateur(Observateur obs);
