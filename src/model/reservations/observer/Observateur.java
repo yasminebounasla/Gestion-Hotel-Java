5d9be357c286a5eb1bde17b51c5568c5b40e7c0a
ikramhadjmohamed <ikramhadjmohamed5@gmail.com>
2025-12-18 18:54:14 +0100

    final update

--- src/model/reservations/observer/Observateur.java
+++ src/model/reservations/observer/Observateur.java
@@ -1,6 +1,17 @@
 package model.Reservations.Observer;
 /**
- * Interface pour les observateurs dans le pattern Observer.
+ * Interface Observateur (pattern Observer).
+ * 
+ * Cette interface représente un observateur qui
+ * surveille un sujet.
+ * 
+ * Un observateur est automatiquement informé
+ * lorsque l’état du sujet change.
+ * 
+ * Dans notre projet, les observateurs servent
+ * à mettre à jour les vues (ex : affichage,
+ * statistiques) après toute modification
+ * des réservations.
  */
 public interface Observateur {
     void mettreAJour(Sujet sujet);
