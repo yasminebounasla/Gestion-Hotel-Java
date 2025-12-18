8abcbf4a8ab9feb2b4035e1eae6e4de011c92ed6
ikramhadjmohamed <ikramhadjmohamed5@gmail.com>
2025-12-18 18:06:22 +0100

    Chambre.java
    final update

--- src/model/Chambres/Composite/Chambre.java
+++ src/model/Chambres/Composite/Chambre.java
@@ -1,7 +1,13 @@
 package model.Chambres.Composite;
 
 import java.util.List;
-
+/**
+ * Classe abstraite représentant une chambre d'hôtel.
+ * 
+ * Chaque chambre possède un numéro, un étage, un prix de base,
+ * un statut (libre, occupée, maintenance) et une liste d'équipements.
+ * Les sous-classes doivent définir la méthode calculerPrix().
+ */
 public abstract class Chambre {
     protected int numero;
     protected int etage;
@@ -9,6 +15,9 @@ public abstract class Chambre {
     protected StatutChambre statut;
     protected List<String> equipements;
 
+     /**
+     * Constructeur protégé pour les sous-classes.
+     */
     protected  Chambre(int numero, int etage, double prixBase, StatutChambre statut, List<String> equipements) {
         this.numero = numero;
         this.etage = etage;
@@ -16,13 +25,20 @@ public abstract class Chambre {
         this.statut = statut;
         this.equipements = equipements;
     }
-
+     /** 
+     * Calcul du prix de la chambre (implémentation spécifique aux sous-classes).
+     */
     public abstract double calculerPrix();
 
+     /**
+     * Affiche les détails de la chambre.
+     */
     public void afficherDetails() {
         System.out.println("Chambre n°" + numero + " | Étage : " + etage + " | Prix de base : " + prixBase + " | Statut : " + statut);
         System.out.println("Équipements : " + equipements);
     }
+    
+    // Getters
     public int getNumero() {
         return numero;
     }
