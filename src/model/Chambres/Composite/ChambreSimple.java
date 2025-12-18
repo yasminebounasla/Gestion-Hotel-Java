9c6f4850f26f3b8b6a98f13243a5e24fa6014bc9
ikramhadjmohamed <ikramhadjmohamed5@gmail.com>
2025-12-18 18:12:39 +0100

    final

--- src/model/Chambres/Composite/ChambreSimple.java
+++ src/model/Chambres/Composite/ChambreSimple.java
@@ -1,13 +1,17 @@
 package model.Chambres.Composite;
 
 import java.util.List;
-
+/**
+ * Représente une chambre simple.
+*/
 public class ChambreSimple extends Chambre {
 
     public ChambreSimple(int numero, int etage, double prixBase, StatutChambre statut, List<String> equipements) {
         super(numero, etage, prixBase, statut, equipements);
     }
-
+    /** 
+     * Prix d'une chambre simple = prix de base
+     */
     @Override
     public double calculerPrix() {
         return prixBase;
