100a2fea974316a329587be24001e23e2bf33ec0
ikramhadjmohamed <ikramhadjmohamed5@gmail.com>
2025-12-18 18:11:21 +0100

    final update

--- src/model/Chambres/Composite/ChambreDouble.java
+++ src/model/Chambres/Composite/ChambreDouble.java
@@ -1,7 +1,9 @@
 package model.Chambres.Composite;
 
 import java.util.List;
-
+ /**
+ * Représente une chambre double.
+ */
 public class ChambreDouble extends Chambre {
 
     public ChambreDouble(int numero, int etage, double prixBase, StatutChambre statut, List<String> equipements) {
@@ -10,6 +12,8 @@ public class ChambreDouble extends Chambre {
 
     @Override
     public double calculerPrix() {
+        // Pour une chambre double, le prix est majoré de 20% par rapport au prix de base
+       // 100% du prix de base + 20% = 120% -> prixBase * 1.2
         return prixBase * 1.2;
     }
 }
